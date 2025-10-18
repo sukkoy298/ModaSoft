102f3904a592b13c6b27b0f9f12ff90f6c8e3f9f
Shinobiah <Shinobiashy@gmail.com>
2025-10-18 16:07:56 -0400

    Añadido el paquete vue router y creadas las rutas para cada uno de los modulos

--- moda-soft/src/main.js
+++ moda-soft/src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // Import our custom CSS
 import './scss/styles.scss'
-
 // Import all of Bootstrap’s JS
 import * as bootstrap from 'bootstrap'
+import router from './router'
 
 createApp(App).mount('#app')
+app.use(router)
+app.mount('#app')
