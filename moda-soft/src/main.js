f67b0ec41bc42c6adc03dd8d3a160ffab1a1cdce
Shinobiah <Shinobiashy@gmail.com>
2025-10-18 15:54:29 -0400

    Añadido Bootstrap 5 con todas sus dependencias al proyecto

--- moda-soft/src/main.js
+++ moda-soft/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap’s JS
+import * as bootstrap from 'bootstrap'
 
 createApp(App).mount('#app')
