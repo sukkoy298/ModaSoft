6ea4616e1f2b10879505860799309896f8f82ac6
sukkoy298 <carlosaug47@gmail.com>
2025-11-08 17:19:42 -0400

    intentando correr el app.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('ModaSoft running\n');
+});
+
+server.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
-
-// importar bootstrap CSS y bundle JS
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
-createApp(App).mount('#app')
-=======
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js';
@@ -16,4 +8,3 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
->>>>>>> origin/Clientes
